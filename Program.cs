231196c2a2389d80c71291059210fad0eb2d8c61
KovDarlin <ki27.kovalenko.daryna@vtc.vn.ua>
2025-07-13 23:18:06 +0300

    done

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter path to file:");
+        string path = Console.ReadLine();
+
+        try
+        {
+            List<int> num = File.ReadAllLines(path)
+                .SelectMany(line => line.Split(' ', ',', '.'))
+                .Where(str => int.TryParse(str, out _))
+                .Select(int.Parse)
+                .ToList();
+
+            int Counting = num.AsParallel().Distinct().Count();
+            Console.WriteLine($"Unique: {Counting}");
+        }
+        catch(Exception problem)
+        {
+            Console.WriteLine("Error: " + problem.Message);
+        }
+    }
+}
